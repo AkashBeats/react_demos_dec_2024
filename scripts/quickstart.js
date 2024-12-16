a09223863f5910815710ab17717b6f55968de845
srivatsan <vatsan_k@hotmail.com>
2024-12-16 20:24:45 +0530

    Day One End of Day commit

---
+++ scripts/quickstart.js
@@ -0,0 +1,13 @@
+var obj = {};
+
+obj.init = function () {
+  var header = document.getElementById("title");
+
+  header.innerHTML = "Sri Real Estate";
+
+  //   header.addEventListener("click", function () {
+  //     header.innerHTML = "Sri Real Estate";
+  //   });
+};
+
+obj.init();
