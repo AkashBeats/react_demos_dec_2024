2752a0b63dfe526d3a94a22111e15dad7547cb81
srivatsan <vatsan_k@hotmail.com>
2024-12-17 11:08:37 +0530

    Day Two First Session

--- scripts/quickstart.js
+++ scripts/quickstart.js
@@ -1,9 +1,29 @@
+// function show(){
+
+// }
+
 var obj = {};
 
+obj.handleClick = function (arg) {
+
+  return function(){
+
+    var headLevelThree = document.querySelector("h3");
+
+    headLevelThree.innerHTML = arg
+  }
+;
+};
 obj.init = function () {
   var header = document.getElementById("title");
 
   header.innerHTML = "Sri Real Estate";
+  header.style.textAlign = "center";
+  header.setAttribute("class", "blue-color");
+
+  var btnOneRef = document.querySelector("#btnOne");
+
+  btnOneRef.addEventListener("click", obj.handleClick("Chennai-50"));
 
   //   header.addEventListener("click", function () {
   //     header.innerHTML = "Sri Real Estate";
